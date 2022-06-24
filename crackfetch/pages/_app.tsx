f09f6f2858db8fa3c3b71442719d0e7680920fb8
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-24 11:37:10 -0500

    added basic files for layout component structure

--- crackfetch/pages/_app.tsx
+++ crackfetch/pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
+import { NotificationsProvider } from '@mantine/notifications'
+import Layout from '../src/components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Head>
+        <title>Crack Fetch</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="icon" type="image/x-icon" href=""></link>
+      </Head>
+      <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </NotificationsProvider>
+      </MantineProvider>
+    </>
+  )
 }
 
 export default MyApp
